6a5d5a349ea40af98b5287df735e1e38f7d93985
Ronney Arias <ronneyarias@gmail.com>
2023-09-27 11:55:38 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+  }
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+  let newCats = [...cats, name];
+  return newCats;
+}
+
+function removeLastCat() {
+    let newCats = cats.slice();
+    newCats.pop();
+    return newCats;
+  }
+
+function prependCat(name) {
+    let newCats = [name, ...cats];
+    return newCats;
+  }
+
+function removeFirstCat() {
+    let newCats = cats.slice(1);
+    return newCats;
+  }
